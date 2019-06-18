5e03bd5bc203f06e9510f212d2e98f30350b5c59
Saul Solano <saul3d@yahoo.com>
2019-06-17 19:22:41 -0500

    added goats

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-danger">Goat</button>
-      </header>
-      
-    </div>
-  );
+import goats from './goats';
+
+import './App.scss';
+
+class App extends React.Component{
+  state = {
+    goats:[], 
+  }
+
+  componentDidMount(){
+    this.setState({ goats })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>Goat Yoga </div>
+        {/* <GoatCorral goats={goats}/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
